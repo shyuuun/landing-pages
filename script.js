06672f2ab21ab7c0e3ce564a0e5fa1fc6738484a
shyuuun <m4rshm4ll0w14@gmail.com>
2024-02-06 01:54:29 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const lineHeader = document.getElementById('line-header'); 
+const leftImage = document.getElementById('left-image');
+const rightImage = document.getElementById('right-image');
+
+
+console.log(lineHeader);
+console.log(leftImage);
+console.log(rightImage);
